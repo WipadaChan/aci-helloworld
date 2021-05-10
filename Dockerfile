1f5faedb8fa480429576d2ffdc1047a5dd211753
WipadaChan <bizzpicnic@gmail.com>
2021-05-10 11:49:14 +0700

    update docker

--- Dockerfile
+++ Dockerfile
@@ -1,6 +1,30 @@
 FROM node:8.9.3-alpine
 RUN mkdir -p /usr/src/app
-COPY ./app/* /usr/src/app/
+#COPY ./app/* /usr/src/app/
+#WORKDIR /usr/src/app
+#RUN npm install
+#CMD node /usr/src/app/index.js
+COPY ./app/mirrorstart.sh /usr/src/app/mirrorstart.sh
 WORKDIR /usr/src/app
-RUN npm install
-CMD node /usr/src/app/index.js
+ENTRYPOINT ["/usr/src/app/mirrorstart.sh",  "$DEST_CON_STR"]
+
+
+# Install OpenJDK-8
+RUN apt-get update && \
+    apt-get install -y openjdk-8-jdk && \
+    apt-get install -y ant && \
+    apt-get clean;
+# Fix certificate issues
+RUN apt-get update && \
+    apt-get install ca-certificates-java && \
+    apt-get clean && \
+    update-ca-certificates -f;
+
+# Setup JAVA_HOME -- useful for docker commandline
+ENV JAVA_HOME /usr/lib/jvm/java-8-openjdk-amd64/
+RUN export JAVA_HOME
+
+RUN wget http://www-us.apache.org/dist/kafka/2.7.0/kafka_2.13-2.7.0.tgz \
+    tar -xzf kafka_2.13-2.7.0.tgz;
+
+CMD cd kafka_2.13-2.7.0 
---
+++ app/mirrorstart.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+set -e
+parse_dns () {
+        START=`expr index "$1" sb://`
+        END=`expr index "$1" \;`
+        SSTART=$((START+5))
+        #SSTART=$(echo $START + 5 | bc)
+        #SEND=$(echo $END -$SSTART -1 | bc)
+        SEND=$(($END -$SSTART -1))
+        echo `expr substr $1 $SSTART $SEND`
+}
+
+#SOURCE_DNS=$(parse_dns $SOURCE_CON_STR)
+DEST_DNS=$(parse_dns $DEST_CON_STR)
+CONSUMER_CONFIG="bootstrap.servers=52.175.55.32:9092\nrequest.timeout.ms=60000\ngroup.id=eh-mirrormaker-group\nexclude.internal.topics=true;"
+echo -e $CONSUMER_CONFIG > consumer.config
+
+PRODUCER_CONFIG="bootstrap.servers=$DEST_DNS:9093\nclient.id=mirror_maker_producer\nrequest.timeout.ms=60000\nsasl.mechanism=PLAIN\nsecurity.protocol=SASL_SSL\nsasl.jaas.config=org.apache.kafka.common.security.plain.PlainLoginModule required username=\"\$ConnectionString\" password=\"$DEST_CON_STR\";"
+echo -e $PRODUCER_CONFIG > producer.config
+
+bin/kafka-mirror-maker.sh --consumer.config consumer.config --producer.config producer.config --whitelist=".*" --num.streams 8
